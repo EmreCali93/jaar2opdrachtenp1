3ac728541b9dc25375e381abfabb092ea08fe46b
EmreCali93 <95631677+EmreCali93@users.noreply.github.com>
2023-02-08 13:36:48 +0100

    Add files via upload

---
+++ login formulier.php
@@ -0,0 +1,24 @@
+<?php
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<title>Formulier</title>
+</head>
+
+<form id='login' action='login.php' method='post' accept-charset='UTF-8'>
+<fieldset>
+<legend>Login</legend>
+<input type='hidden' name='submitted' id='submitted' value='1'/>
+
+<label for='username' >UserName*:</label>
+<input type='text' name='username' id='username'  maxlength="50"/>
+
+<label for='password' >Password*:</label>
+<input type='password' name='password' id='password' maxlength="50"/>
+
+<input type='submit' name='Submit' value='Submit'/>
+
+</fieldset>
+</form>
+
+?>
