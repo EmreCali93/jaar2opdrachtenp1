d424224c54de956f5ba519e6d259f76547892766
DataVerpalenScorpian <99246618+DataVerpalenScorpian@users.noreply.github.com>
2023-02-10 14:55:43 +0100

    Code aanpassing

--- registratie formulier.php
+++ registratie formulier.php
@@ -1,16 +1,22 @@
-<?php
-<head>
-	<meta http-equiv="content-type" content="text/html; charset=utf-8">
-	<title>Formulier</title>
-</head>
+<!DOCTYPE html>
+<html lang="nl">
+	<head>
+	</head>
 <body>
+
+<h2>Registratie</h2>
 <h3>Op deze pagina kunt u uw gegevens invullen en versturen.</h3>
+
 <form action="FormulierAfhandeling.php" method="post">
+
 <p>Voornaam: <input name="VoorNaam" type="text" size="30" maxlength="80"></p>
 <p>Achternaam: <input name="AchterNaam" type="text" size="30" maxlength="80"></p>
 <p>Email: <input name="EmailAdres" type="text" size="30" maxlength="80"></p>
 <p>Bericht: <textarea name="BeRicht" style="width: 250px; height: 40px;"></textarea></p>
 <p><input name="Submit" type="submit"></p>
 </form>
+
 </body>
-?>
+
+</html>
+
