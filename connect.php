05e07fc5977f667ddf799bc5f937b5c7675edbbd
DataVerpalenScorpian <99246618+DataVerpalenScorpian@users.noreply.github.com>
2023-02-16 14:45:59 +0100

    connect.php

---
+++ connect.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+
+    $server = "localhost";
+    $db = "inlogsysteem";
+    $user = "root";
+    $pass = "";
+
+    $options = [
+        PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE    => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES      => false, 
+    ];
+
+
+    try {
+    $conn = new PDO("mysql:host=$server; dbname=$db", $user, $pass, $options);
+    echo "Connected successfully </br>";
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage() . "</br>" . (int)$e->getCode();
+    }
+
+
+    $conn = null;
+
+
+    ?>
+
+
+</body>
+</html>
