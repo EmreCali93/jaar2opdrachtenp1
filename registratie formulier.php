df9632a2d491d90a23a9b8d336856079cf8a6bcf
DataVerpalenScorpian <99246618+DataVerpalenScorpian@users.noreply.github.com>
2023-02-19 20:27:34 +0100

    Formulier aangepast

--- registratie formulier.php
+++ registratie formulier.php
@@ -7,14 +7,30 @@
 <h2>Registratie</h2>
 <h3>Op deze pagina kunt u uw gegevens invullen en versturen.</h3>
 
-<form>
-<p>Username: <input name="Username" type="text" size="30" maxlength="80"></p>
-<p>Password: <input name="Password" type="text" size="30" maxlength="80"></p>
-<p>Email: <input name="EmailAdres" type="text" size="30" maxlength="80"></p>
-<p><input name="Submit" type="submit"></p>
+<form action="registratievalidatie.php" method="post" id="registratie" novalidate>
+	<div>
+		<label for="name">Name</label>
+		<input type="text" id="name" name="name">
+   </div>
+
+	<div>
+		<label for="email">Email</label>
+		<input type="email" id="email" name="email">
+    </div>
+
+	<div>
+		<label for="password">Password</label>
+		<input type="password" id="password" name="password">
+    </div>
+
+	<div>
+		<label for="password_confirmation">Repeat password</label>
+		<input type="password" id="password_confirmation" name="password_confirmation">
+    </div>
+
+	<button>Sign up</button>
 </form>
 
 </body>
-
 </html>
 
